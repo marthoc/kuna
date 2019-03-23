7e82de62db9ffa40246e055b563bbb34a3df30fc
Mark Coombes <mark@markcoombes.ca>
2019-03-22 21:32:25 -0400

    Fire event with recording data
    Add new method to Kuna account class to fire a HASS event on HASS start and every recording_interval seconds; method queries the Kuna API for available recordings for each camera for the past recording_interval seconds, then fires an event for each retrieved recording with recording data for consumption by other HASS services (e.g. notify, downloader).

--- __init__.py
+++ __init__.py
@@ -12,7 +12,12 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import discovery
 from homeassistant.helpers.event import async_track_time_interval
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from homeassistant.const import (
+    CONF_EMAIL,
+    CONF_EVENT,
+    CONF_PASSWORD,
+    EVENT_HOMEASSISTANT_START,
+)
 
 REQUIREMENTS = ["pykuna==0.5.0"]
 
@@ -20,9 +25,11 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "kuna"
 
+CONF_RECORDING_INTERVAL = "recording_interval"
 CONF_STREAM_INTERVAL = "stream_interval"
 CONF_UPDATE_INTERVAL = "update_interval"
 
+DEFAULT_RECORDING_INTERVAL = 7200
 DEFAULT_STREAM_INTERVAL = 5
 DEFAULT_UPDATE_INTERVAL = 15
 
@@ -33,10 +40,13 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_EMAIL): cv.string,
                 vol.Required(CONF_PASSWORD): cv.string,
                 vol.Optional(
-                    CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL
+                    CONF_RECORDING_INTERVAL, default=DEFAULT_RECORDING_INTERVAL
                 ): cv.time_period_seconds,
                 vol.Optional(
                     CONF_STREAM_INTERVAL, default=DEFAULT_STREAM_INTERVAL
+                ): cv.time_period_seconds,
+                vol.Optional(
+                    CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL
                 ): cv.time_period_seconds,
             }
         )
@@ -60,10 +70,18 @@ async def async_setup(hass, config):
 
     email = config[DOMAIN][CONF_EMAIL]
     password = config[DOMAIN][CONF_PASSWORD]
+    recording_interval = config[DOMAIN][CONF_RECORDING_INTERVAL]
     stream_interval = config[DOMAIN][CONF_STREAM_INTERVAL]
     update_interval = config[DOMAIN][CONF_UPDATE_INTERVAL]
 
-    kuna = KunaAccount(email, password, async_get_clientsession(hass), stream_interval)
+    kuna = KunaAccount(
+        hass,
+        email,
+        password,
+        async_get_clientsession(hass),
+        recording_interval,
+        stream_interval,
+    )
 
     try:
         await kuna.account.authenticate()
@@ -84,9 +102,15 @@ async def async_setup(hass, config):
     hass.data[DOMAIN] = kuna
 
     for component in KUNA_COMPONENTS:
-        hass.async_create_task(discovery.async_load_platform(hass, component, DOMAIN, {}, config))
+        hass.async_create_task(
+            discovery.async_load_platform(hass, component, DOMAIN, {}, config)
+        )
 
     async_track_time_interval(hass, kuna.update, update_interval)
+
+    async_track_time_interval(hass, kuna.scan_for_recordings, recording_interval)
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, kuna.scan_for_recordings)
 
     async def enable_notifications(call):
         serial_number = call.data.get(ATTR_SERIAL_NUMBER)
@@ -141,10 +165,15 @@ async def async_setup(hass, config):
 
 class KunaAccount:
     """Represents a Kuna account."""
-    def __init__(self, email, password, websession, stream_interval):
+
+    def __init__(
+        self, hass, email, password, websession, recording_interval, stream_interval
+    ):
         from pykuna import KunaAPI
 
+        self._hass = hass
         self.account = KunaAPI(email, password, websession)
+        self._recording_interval = recording_interval
         self.stream_interval = stream_interval
         self._update_listeners = []
 
@@ -162,3 +191,37 @@ class KunaAccount:
 
     def add_update_listener(self, listener):
         self._update_listeners.append(listener)
+
+    async def scan_for_recordings(self, *_):
+        """
+        Queries the Kuna API for recordings for each camera in the account.
+        Fires an event with the recording data for consumption by other services.
+        """
+        _LOGGER.debug("Scanning for Kuna recordings.")
+        recordings = []
+        for camera in self.account.cameras.values():
+            recs = await camera.get_recordings_by_time(self._recording_interval)
+            for item in recs:
+                recordings.append(item)
+
+        for recording in recordings:
+            url = await recording.get_download_link()
+
+            if url is not None:
+                event_data = {
+                    "category": "recording",
+                    "serial_number": recording.camera["serial_number"],
+                    "label": recording.label,
+                    "timestamp": recording.timestamp,
+                    "duration": recording.duration,
+                    "url": url,
+                }
+                self._hass.bus.async_fire(
+                    "{}_{}".format(DOMAIN, CONF_EVENT), event_data
+                )
+            else:
+                _LOGGER.error(
+                    "Error retrieving download url for Kuna recording: {} ({})".format(
+                        recording.label, recording.camera["serial_number"]
+                    )
+                )
