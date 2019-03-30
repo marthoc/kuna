c25b33094153a860814665c0e3d9ea39665545f4
Mark Coombes <mark@markcoombes.ca>
2019-03-30 17:09:47 -0400

    Bump pykuna to 0.5.1

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ from homeassistant.const import (
     EVENT_HOMEASSISTANT_START,
 )
 
-REQUIREMENTS = ["pykuna==0.5.0"]
+REQUIREMENTS = ["pykuna==0.5.1"]
 
 _LOGGER = logging.getLogger(__name__)
 
